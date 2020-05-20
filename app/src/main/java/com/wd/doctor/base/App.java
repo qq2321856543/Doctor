599231367b98e75c43963b99b27e5e4c50bf86a2
Administrator <2321856543@qq.com>
2020-05-20 17:15:32 +0800

    Buttkif

--- app/src/main/java/com/wd/doctor/base/App.java
+++ app/src/main/java/com/wd/doctor/base/App.java
@@ -1,7 +1,14 @@
 package com.wd.doctor.base;
 
 
+import android.support.multidex.MultiDex;
+
 import com.wd.common.Base.BaseApplication;
 
 public class App extends BaseApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MultiDex.install(this);
+    }
 }
